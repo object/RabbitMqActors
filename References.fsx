fabb8ca1299843afb8959e59306f562fa2da038f
carstenj <carstenj@gmail.com>
2016-06-01 21:33:22 +0200

    Ensure right RabbitMQ client library is loaded on OSX/Mono

--- References.fsx
+++ References.fsx
@@ -1,12 +1,14 @@
-﻿#I @".\packages\RabbitMQ.Client.3.6.2\lib\net45"
-#I @".\packages\Newtonsoft.Json.7.0.1\lib\net45"
-#I @".\packages\Akka.1.0.8\lib\net45"
-#I @".\packages\Akka.FSharp.1.0.8\lib\net45"
-#I @".\packages\FsPickler.1.2.21\lib\net45"
-#I @".\packages\System.Collections.Immutable.1.1.36\lib\portable-net45+win8+wp8+wpa81" 
+﻿#I @"./packages/Newtonsoft.Json.7.0.1/lib/net45"
+#I @"./packages/Akka.1.0.8/lib/net45"
+#I @"./packages/Akka.FSharp.1.0.8/lib/net45"
+#I @"./packages/FsPickler.1.2.21/lib/net45"
+#I @"./packages/System.Collections.Immutable.1.1.36/lib/portable-net45+win8+wp8+wpa81" 
 
+// Mono has a build in version of RabbitMQ that takes
+// precedance unless with fully quality the path to the client
+// library that we really want to load
+#r "./packages/RabbitMQ.Client.3.6.2/lib/net45/RabbitMQ.Client.dll"
 #r "System.Collections.Immutable.dll"
-#r "RabbitMQ.Client.dll"
 #r "Akka.dll"
 #r "Akka.FSharp.dll"
 
